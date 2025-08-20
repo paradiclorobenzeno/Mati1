c9fae9d3af3ffdc27e07302d63178c0c7573b12c
paradiclorobenzeno <t.ignaciofq12@gmail.com>
2025-08-20 16:38:48 -0400

    Prueba

--- exp/area_2d.gd
+++ exp/area_2d.gd
@@ -1,5 +1,13 @@
 extends Area2D
 
+var click = false
 
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	pass # Replace with function body.
+func _process(_delta):
+	if click:
+		position = get_global_mouse_position()
+
+func _on_input_event(_viewport, _event, _shape_idx) -> void:
+	if Input.is_action_pressed("left_click"):
+		click = true
+	if Input.is_action_just_released("left_click"):
+		click = false
